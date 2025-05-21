fae9fb9b5e32e2628bd54ae8ae675496faf6f357
Emman Mlmb <emmanjibi@gmail.com>
2025-05-21 15:03:25 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import StepCard from '../components/StepCard';
+import '../styles/globals.css';
 
 const steps = [
   {
