c0d73db0a7c9b2d166b9300ac5d263d3b3e0f072
Emman Mlmb <emmanjibi@gmail.com>
2025-05-21 14:58:14 +0100

    Delete pages/_app.tsx

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// Import du CSS global (inclut Tailwind)
-import '../styles/globals.css';
-
-// Fonction principale qui rend chaque page
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
